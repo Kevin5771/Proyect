34eb0909733f7837cd183e9e5906de5330884a6b
Kevin5771 <kevin577gutierrez@gmail.com>
2024-10-20 20:45:46 -0600

    Inyección de Dependencias

--- app/src/main/java/com/albrodiaz/gestvet/MainActivity.kt
+++ app/src/main/java/com/albrodiaz/gestvet/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.albrodiaz.gestvet.ui.theme.GestVet2Theme
+import com.albrodiaz.gestvet.ui.theme.GestVetTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GestVet2Theme {
+            GestVetTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    GestVet2Theme {
+    GestVetTheme {
         Greeting("Android")
     }
 }
